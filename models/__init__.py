d40749fde2f455b2f89449aafd30e07d1e9bbb13
Rahmat <rahmatbola3@gmail.com>
2024-03-21 04:09:42 +0000

    corrections

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 """Module init for models module"""
 from os import getenv
-from .base_model import BaseModel
-
-name2class = {
-    "BaseModel": BaseModel,
-    "User": User,
-    "Place": Place,
-    "City": City,
-    "State": State,
-    "Amenity": Amenity,
-    "Review": Review
-}
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
@@ -21,3 +10,4 @@ else:
     storage = FileStorage()
 
 storage.reload()
+
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,7 @@ connection.
 """
 
 import os
-from models.base_model import Base
+from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -13,6 +13,7 @@ from models.review import Review
 from models.user import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+
 name2class = {
     'Amenity': Amenity,
     'City': City,
